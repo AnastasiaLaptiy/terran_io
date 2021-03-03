dd24f7946b2c8e9a69c2cb6556a376f4b831b361
Anastasiia <anastasiia.laptii@tech-stack.io>
2021-03-03 13:30:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import click
+import os
+from datetime import datetime
+
+from pathlib import Path
+from scipy.spatial.distance import cosine
+
+from terran.face import extract_features, face_detection
+from terran.io import open_image, resolve_images
+from terran.vis import display_image, vis_faces
+
+threshold = 0.5
+display = True
+imagePath = open_image('https://crowdpiclivestorage.blob.core.windows.net/photos-preview/e76eddc8-149a-41ac-a0fc-59f1e0bff904_preview_desktop.jpg')
+faces_in_reference = face_detection(imagePath)
+
+ref_feature = extract_features(imagePath, faces_in_reference[0])
+path = os.path.abspath(os.getcwd()) + "\src\input_photos"
+click.echo(path)
+paths = resolve_images(path, batch_size = 1)
+
+for batch_paths in paths:
+        batch_images = list(map(open_image, batch_paths))
+        
+        faces_per_image = face_detection(batch_images)      
+        # for path, face in zip(batch_paths, faces_per_image):
+        #         face_path = os.path.join("../terran_io/src/detected_faces/", datetime.now().strftime('%Y-%m-%d %H%M%S.%f') + ".json")    
+        #         file_faces = open(face_path, "a")
+        #         file_faces.write(str(path) + "\n")
+        #         file_faces.write(str(face))  
+                    
+        features_per_image = extract_features(batch_images, faces_per_image)         
+        # for path, feature in zip(batch_paths, features_per_image):
+        #         feature_path = os.path.join("../terran_io/src/features/", datetime.now().strftime('%Y-%m-%d %H%M%S.%f') + ".json")    
+        #         file_features = open(feature_path, "a")
+        #         file_features.write(str(path) + "\n")
+        #         file_features.write(str(feature))
+
+        for path, image, faces, features in zip(
+            batch_paths, batch_images, faces_per_image, features_per_image
+        ):
+            for face, feature in zip(faces, features):            
+                confidence = cosine(ref_feature, feature)
+                if confidence < threshold:
+                    click.echo(f'{path}, confidence = {confidence:.2f}')
+                    if display:
+                        display_image(vis_faces(image, face))
+                        
---
+++ readme.md
@@ -0,0 +1,12 @@
+Setup
+1. install Python v.3.9
+2. install Anaconda https://www.anaconda.com/products/individual
+3. Run command bellow
+   pip install scikit-build;
+   pip install terran;
+
+How to solve Pytorch problem?
+https://discuss.pytorch.org/t/importerror-key-already-registered-with-the-same-priority/86271
+
+How to run? 
+& path_to_python.exe path_to_main.py
